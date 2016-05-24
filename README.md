73a35214177b6a8c2d08f4b281dab62e5e80fa2b
Kento Oriya <k-oriya@mksc.jp>
2016-05-24 18:25:07 +0900

    move explanation to readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# arrange_excel
+# arrange_excel.exe.rb
+
+## ファイル種別
+
+実行ファイル
+
+## 必須
+
+- Windows OS
+- Excelアプリケーション
+- arrange_excel.rb(同じディレクトリに配置する)
+- Rubyが実行できる環境(Rubyコマンドプロンプト、Rubyを入れたCygwin等)
+
+## コマンド例
+
+```
+ruby arrange_excel.exe.rb
+```
+->実行フォルダ内のファイルをすべて処理する
+
+```
+ruby arrange_excel.exe.rb file1.xlsx file2.xls dir/
+```
+->指定ファイル、または指定ディレクトリ内のファイルすべてを処理する
+
+```
+ruby arrange_excel.exe.rb -r dir/
+```
+->指定したディレクトリをサブディレクトリも含め、再帰的に処理する
+
+```
+ruby arrange_excel.exe.rb -v
+```
+->実行フォルダ内のファイルをすべて処理し、処理経過を表示する
+
--- lib/arrange_excel.exe.rb
+++ lib/arrange_excel.exe.rb
@@ -1,29 +1,4 @@
-﻿require_relative 'arrange_excel.rb'
-
-=begin
-
-=== 説明 ===
-
-#必須
-- Windows OS
-- Excelアプリケーション
-- arrange_excel.rb(同じディレクトリに配置する)
-- Rubyが実行できる環境(Rubyコマンドプロンプト、Rubyを入れたCygwin等)
-
-#コマンド例
-ruby arrange_excel.exe.rb
-->実行フォルダ内のファイルをすべて処理する
-
-ruby arrange_excel.exe.rb file1.xlsx file2.xls dir/
-->指定ファイル、または指定ディレクトリ内のファイルすべてを処理する
-
-ruby arrange_excel.exe.rb -r dir/
-->指定したディレクトリをサブディレクトリも含め、再帰的に処理する
-
-ruby arrange_excel.exe.rb -v
-->実行フォルダ内のファイルをすべて処理し、処理経過を表示する
-
-=end
+require_relative 'arrange_excel.rb'
 
 include ArrangeExcel
 
