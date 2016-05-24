f64e5d26ca4a1079c65cfb7fc0c3eb0751ddc961
Kento Oriya <k-oriya@mksc.jp>
2016-05-24 18:03:40 +0900

    first commit

---
+++ lib/arrange_excel.exe.rb
@@ -0,0 +1,54 @@
+﻿require_relative 'arrange_excel.rb'
+
+=begin
+
+=== 説明 ===
+
+#必須
+- Windows OS
+- Excelアプリケーション
+- arrange_excel.rb(同じディレクトリに配置する)
+- Rubyが実行できる環境(Rubyコマンドプロンプト、Rubyを入れたCygwin等)
+
+#コマンド例
+ruby arrange_excel.exe.rb
+->実行フォルダ内のファイルをすべて処理する
+
+ruby arrange_excel.exe.rb file1.xlsx file2.xls dir/
+->指定ファイル、または指定ディレクトリ内のファイルすべてを処理する
+
+ruby arrange_excel.exe.rb -r dir/
+->指定したディレクトリをサブディレクトリも含め、再帰的に処理する
+
+ruby arrange_excel.exe.rb -v
+->実行フォルダ内のファイルをすべて処理し、処理経過を表示する
+
+=end
+
+include ArrangeExcel
+
+argv = ARGV.map{|e| e.downcase}
+
+sub_dir = !argv.delete('-r').nil?
+log     = !argv.delete('-v').nil?
+
+extensions = %w(xls xlsx xlsm)
+
+commands = argv.empty? ? ['./'] : ARGV
+
+file_names = commands.inject([]) do |files, command|
+               if File.file?(command) && extensions.map{|e| '.' + e}.include?(File.extname(command))
+                 files + Dir.glob(command)
+               elsif File.directory? command
+                 path = if command[-1] != '/'
+                          command + '/'
+                        else
+                          command
+                        end
+                 files + Dir.glob(path + (sub_dir ? "**/" : "") + "*.{#{extensions.join(',')}}")
+               else
+                 files
+               end
+             end
+
+arrange_excel_files file_names, log
---
+++ lib/arrange_excel.rb
@@ -0,0 +1,77 @@
+﻿require 'win32ole'
+
+class WIN32OLE
+  include Enumerable
+end
+
+module ArrangeExcel
+
+  # シートのカーソルをA1に合わせます
+  # 左上にスクロールします
+  # 拡大率を100%にします
+  def arrange_worksheet!(sheet, log = false)
+    visible = sheet.visible
+    sheet.visible = -1
+    sheet.activate
+    sheet.cells(1, 1).activate
+    window = sheet.parent.windows(1)
+    window.zoom = 100
+    window.scrollRow = 1
+    window.scrollColumn = 1
+    sheet.visible = visible
+    puts("success:#{sheet.parent.name}/#{sheet.name}") if log
+  end
+
+  def arrange_worksheet(sheet, log = false)
+    success = false
+    arrange_worksheet! sheet, log
+    success = true
+  rescue WIN32OLERuntimeError
+    puts("failure:#{sheet.parent.name}/#{sheet.name}") if log
+  ensure
+    return success
+  end
+
+  # ブック内のすべてのシートを整え、最初のシートをアクティブにします
+  def arrange_workbook!(book, log = false)
+    book.worksheets.reverse_each do |sheet|
+      arrange_worksheet! sheet, log
+    end
+  end
+
+  def arrange_workbook(book, log = false)
+    book.worksheets.reverse_each do |sheet|
+      arrange_worksheet sheet, log
+    end
+  end
+
+  # 指定したファイルを整えます
+  def arrange_excel_files(file_names, log = false)
+    excel = WIN32OLE.new 'Excel.Application'
+    excel.DisplayAlerts = false
+    file_names.each do |file_name|
+      fso = WIN32OLE.new 'Scripting.FileSystemObject'
+      absolute_path = fso.GetAbsolutePathName file_name
+      puts("file:#{absolute_path}") if log
+      
+      book = excel.Workbooks.Open absolute_path
+      
+      arrange_workbook book, log
+      
+      book.save
+      book.close
+    end
+  rescue WIN32OLERuntimeError
+    puts 'Please close this workbook.'
+  rescue => e
+    puts e
+  ensure
+    excel.Workbooks.Close
+    excel.quit
+  end
+
+  def arrnge_excel_file(file_name, log = false)
+    arrange_excel_files([file_name], log)
+  end
+  
+end
